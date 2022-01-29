65243ec8b7bf1a572252b120bc036c028a8ea6f8
MrDiver <mrdiverlp@gmail.com>
2022-01-29 20:44:02 +0100

    Extended Option Model

--- symbolic/Game.py
+++ symbolic/Game.py
@@ -2,6 +2,8 @@
 from enum import Enum
 import pygame as pg
 from pygame.locals import *
+import random as rng
+import numpy as np
 vec = pg.math.Vector2
 
 
@@ -21,26 +23,31 @@ class Collider(pg.sprite.Sprite):
 class Player(pg.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
-        tileset = pg.image.load("res/tiles.png")
-        self.surf = pg.Surface((16, 16))
-        self.surf.fill((0, 0, 0))
-        self.surf.set_colorkey((0, 0, 0))
-        self.surf.blit(tileset, dest=pg.Rect(
-            0, 0, 16, 16), area=pg.Rect(6*16, 15*16, 16, 16))
+        self.surf = self._create_surface()
         self.rect: Rect = self.surf.get_rect()
         self.pos = vec((position[0]*16+16, position[1]*16+16))
         self._update()
         self.walls = None
+        self.step_size = rng.randint(1, 3)
+
+    def _create_surface(self) -> pg.SurfaceType:
+        tileset = pg.image.load("res/tiles.png")
+        surf: pg.SurfaceType = pg.Surface((16, 16))
+        surf = pg.Surface.convert_alpha(surf)
+        surf.set_colorkey((0, 0, 0))
+        surf.blit(tileset, dest=pg.Rect(
+            0, 0, 16, 16), area=pg.Rect(6*16, 15*16, 16, 16))
+        return surf
 
     def collide(self, dir: Direction) -> bool:
         if dir is Direction.LEFT:
-            self.rect.centerx = self.pos.x - 1
+            self.rect.centerx = self.pos.x - self.step_size
         elif dir is Direction.RIGHT:
-            self.rect.centerx = self.pos.x + 1
+            self.rect.centerx = self.pos.x + self.step_size
         elif dir is Direction.UP:
-            self.rect.centery = self.pos.y + 1
+            self.rect.centery = self.pos.y + self.step_size
         elif dir is Direction.DOWN:
-            self.rect.centery = self.pos.y - 1
+            self.rect.centery = self.pos.y - self.step_size
 
         collided = pg.sprite.spritecollide(
             Collider(self.rect), self.walls, False)
@@ -50,17 +57,26 @@ class Player(pg.sprite.Sprite):
     def move(self, dir: Direction) -> bool:
         if not self.collide(dir):
             if dir is Direction.LEFT:
-                self.pos.x -= 1
+                self.pos.x -= self.step_size
             elif dir is Direction.RIGHT:
-                self.pos.x += 1
+                self.pos.x += self.step_size
             elif dir is Direction.UP:
-                self.pos.y += 1
+                self.pos.y += self.step_size
             elif dir is Direction.DOWN:
-                self.pos.y -= 1
+                self.pos.y -= self.step_size
 
             self._update()
+            self._update_step()
             return True
         return False
+
+    def _update_step(self):
+        self.step_size = rng.randint(1, 3)
+
+    def set_state(self, x, y):
+        self.pos.x = x
+        self.pos.y = y
+        self.rect.center = self.pos
 
     def _update(self):
         self.rect.center = self.pos
@@ -96,9 +112,10 @@ class MiniGame:
     def __init__(self,) -> None:
         pg.init()
         self.fps = pg.time.Clock()
-        self.WIDTH, self.HEIGHT = 720, 720
-        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.WIDTH, self.HEIGHT = 720*2, 720*2
+        self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT), 0, 32)
         self.tmp_screen = pg.Surface((320, 320))
+        self.tmp_full_screen = pg.Surface((self.WIDTH, self.HEIGHT), 0, 32)
         # creating objects
         self.player = Player((2, 2))
         # Adding sprites to game
@@ -106,24 +123,24 @@ class MiniGame:
         self.walls = pg.sprite.Group()
 
         map_array = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                     [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
         for y in range(len(map_array)):
@@ -145,10 +162,66 @@ class MiniGame:
 
     def draw(self):
         self.tmp_screen.fill((0, 0, 0))
-
+        # print("Drawing")
         for entity in self.sprites:
             self.tmp_screen.blit(entity.surf, entity.rect)
         pg.transform.scale(
             self.tmp_screen, (self.WIDTH, self.HEIGHT), self.screen)
         pg.display.update()
         self.fps.tick()
+
+    def draw_background(self):
+        self.tmp_screen.fill((0, 0, 0))
+        for entity in self.sprites:
+            if entity is not self.player:
+                self.tmp_screen.blit(entity.surf, entity.rect)
+        pg.transform.scale(
+            self.tmp_screen, (self.WIDTH, self.HEIGHT), self.screen)
+        pg.display.update()
+        self.fps.tick()
+
+    def overlay(self, color, alpha):
+        self.tmp_screen.fill((0, 0, 0, 0))
+        self.tmp_full_screen.fill((0, 0, 0, 0))
+        self.tmp_full_screen.set_colorkey((0, 0, 0))
+        surf: pg.Surface = self.player._create_surface()
+        surfcolored = surf.copy()
+        surfcolored.fill(color)
+        surfcolored.set_alpha(alpha)
+        surfcolored.blit(surf, (0, 0))
+        self.tmp_screen.blit(surfcolored, self.player.rect)
+        pg.transform.scale(
+            self.tmp_screen, (self.WIDTH, self.HEIGHT), self.tmp_full_screen)
+        self.tmp_full_screen.set_alpha(alpha)
+        self.screen.blit(self.tmp_full_screen, (0, 0))
+        pg.display.update()
+
+    def overlay_transition(self, start, end, start_color, end_color, alpha):
+        self.tmp_screen.fill((0, 0, 0, 0))
+        self.tmp_full_screen.fill((0, 0, 0, 0))
+        self.tmp_full_screen.set_colorkey((0, 0, 0))
+
+        _xs_min = min(start[0], end[0])
+        _xs_max = max(start[0], end[0])
+        xs = np.arange(_xs_min, _xs_max+1, 1)
+        _ys_min = min(start[1], end[1])
+        _ys_max = max(start[1], end[1])
+        ys = np.arange(_ys_min, _ys_max+1, 1)
+
+        # print(_ys)
+        if len(xs) > len(ys):
+            ys = np.interp(xs, [_xs_min, _xs_max], [_ys_min, _ys_max])
+        else:
+            xs = np.interp(ys, [_ys_min, _ys_max], [_xs_min, _xs_max])
+            # ys = np.interp(_ys, start[1], end[1])
+        points = np.transpose([xs, ys])
+
+        colors = np.linspace(start_color, end_color, len(points))
+
+        for i in range(len(points)-1):
+            pg.draw.aaline(self.tmp_screen, colors[i], points[i], points[i+1])
+        pg.transform.scale(
+            self.tmp_screen, (self.WIDTH, self.HEIGHT), self.tmp_full_screen)
+        self.tmp_full_screen.set_alpha(alpha)
+        self.screen.blit(self.tmp_full_screen, (0, 0))
+        pg.display.update()
--- symbolic/main.py
+++ symbolic/main.py
@@ -1,48 +1,105 @@
-
+"""
+Main Module
+"""
 import time
-from Game import MiniGame, Direction
-import numpy as np
 from collections import namedtuple
-from typing import Callable, List
-
+from typing import Callable, List, TypeAlias, Any
+from Game import MiniGame, Direction, Player
+import numpy as np
+from nptyping import NDArray, Bool, Float
+import random as rng
 
 OptionTransition = namedtuple('OptionTransition', ['start', 'dest'])
 """[summary]
     state - refers to the new state after the option is executed
-    executed - true iff the option could be executed i.e. state was in initiation set
+    executed - true iff the option could be executed
+    i.e. state was in initiation set
 """
 OptionExecuteReturn = namedtuple(
-    'OptionExecuteReturn', ['state', 'executed'])
+    'OptionExecuteReturn', ['state', 'executed', 'time'])
+
+
+LowLevelState: TypeAlias = NDArray[Float]
+LowLevelStates: TypeAlias = NDArray[Float]
+SymbolicState: TypeAlias = NDArray[Bool]
+StateTransform: TypeAlias = Callable[[LowLevelState], LowLevelState]
+StateTest: TypeAlias = Callable[[LowLevelState], bool]
+StateConsumer: TypeAlias = Callable[[LowLevelState], Any]
 
 
 class Option:
-    def __init__(self, name: str, init_test: Callable[[np.ndarray], bool], policy: Callable[[np.ndarray], np.ndarray], termination: Callable[[np.ndarray], int]) -> None:
+    def __init__(self, name: str,
+                 policy: StateTransform,
+                 init_test: StateTest,
+                 termination: StateConsumer) -> None:
         self.name = name
         self.init_test = init_test
         self.policy = policy
         self.termination = termination
 
-        self.initiation_set = np.array([])
+        self.initiation_set: List[LowLevelState] = []
+        self.effect_set: List[LowLevelState] = []
         self.transitions: List[OptionTransition] = []
 
-    def execute(self, state: np.ndarray, update_callback: Callable[[], None]) -> OptionExecuteReturn:
-        if self.init_test(state):
-            return OptionExecuteReturn(state, False)
+    def _add_transition(self, start: LowLevelState, dest: LowLevelState):
+        self.initiation_set.append(start)
+        self.effect_set.append(dest)
+        self.transitions.append(OptionTransition(start, dest))
 
+    def effect(self) -> LowLevelStates:
+        return np.unique(self.effect_set, axis=0)
+
+    def initiation(self) -> LowLevelStates:
+        return np.unique(self.initiation_set, axis=0)
+
+    def execute(self, state: LowLevelState,
+                update_callback: Callable[[LowLevelState], None]) -> OptionExecuteReturn:
+        if self.init_test(state):
+            return OptionExecuteReturn(state, False, 0)
         terminated = False
+        start_state = state
+        start_time = time.time()
         while not terminated:
             state = self.policy(state)
             terminated = self.termination(state) > 0
+            update_callback(state)
 
-        return OptionExecuteReturn(state, True)
+        self._add_transition(start_state, state)
+
+        return OptionExecuteReturn(state, True, time.time()-start_time)
 
 
 class PropositionSymbol:
-    def __init__(self, name: str, test: Callable[[np.ndarray], bool]) -> None:
+    def __init__(self, name: str,
+                 test: StateTest) -> None:
         self.name = name
         self.test = test
+        self.low_level_states: List[LowLevelState] = []
 
-    # Add operators or, and, not, null
+    def p_test(self, state: LowLevelState):
+        res = self.test(state)
+        if res:
+            self.low_level_states.append("Hallo")
+        return res
+
+    def p_and(self, other):
+        return __make_symbol("("+self.name+"&"+other.name+")",
+                             (lambda x: self.test(x) and other.test(x)))
+
+    def p_or(self, other):
+        return __make_symbol("("+self.name+"|"+other.name+")",
+                             (lambda x: self.test(x) or other.test(x)))
+
+    def p_not(self):
+        return __make_symbol("not_"+self.name, (lambda x: not self.test(x)))
+
+    def p_null(self) -> bool:
+        return len(self.low_level_states) == 0
+
+
+def __make_symbol(name: str,
+                  test: StateTest) -> PropositionSymbol:
+    return PropositionSymbol(name, test)
 
 
 class Plan:
@@ -56,23 +113,126 @@ class Playroom:
     def __init__(self) -> None:
         self.game = MiniGame()
 
-    def get_state(self) -> np.ndarray:
+    def get_state(self) -> LowLevelState:
         return np.array([self.game.player.pos.x,
                          self.game.player.pos.y])
 
+    def set_state(self, s: LowLevelState):
+        self.game.player.set_state(s[0], s[1])
+
+    def execute_no_change(self, s: LowLevelState, f: Callable[[], Any]) -> LowLevelState:
+        old_state = self.get_state()
+        self.set_state(s)
+        f()
+        new_state = self.get_state()
+        self.set_state(old_state)
+        return new_state
+
+    def execute_no_change_return(self, s: LowLevelState, f: Callable[[], Any]) -> LowLevelState:
+        old_state = self.get_state()
+        self.set_state(s)
+        res = f()
+        self.set_state(old_state)
+        return res
+
     def update(self) -> None:
         self.game.draw()
+
+    def draw_state(self, s: LowLevelState):
+        old_state = self.get_state()
+        self.set_state(s)
+        self.game.draw()
+        self.set_state(old_state)
+
+    def draw_background(self):
+        self.game.draw_background()
+
+    def overlay_state(self, s: LowLevelState, color=(255, 0, 0)):
+        old_state = self.get_state()
+        self.set_state(s)
+        self.game.overlay(color, 128)
+        self.set_state(old_state)
+
+    def overlay_transition(self, start: LowLevelState, end: LowLevelState, start_color=(255, 0, 0), end_color=(0, 255, 0)):
+        self.game.overlay_transition(
+            (start[0], start[1]), (end[0], end[1]), start_color, end_color, 50)
+
+
+class Agent:
+    def __init__(self, options: List[Option]):
+        self.options: List[Option] = options
+
+
+class PlayroomAgent(Agent):
+    def __init__(self, playroom: Playroom, player: Player):
+        super().__init__([Option("MoveLeft",
+                                 self._move(Direction.LEFT),
+                                 self._move_test(Direction.LEFT),
+                                 self._move_terminate(Direction.LEFT)
+                                 ),
+                          Option("MoveRight",
+                                 self._move(Direction.RIGHT),
+                                 self._move_test(Direction.RIGHT),
+                                 self._move_terminate(Direction.RIGHT)
+                                 ),
+                          Option("MoveUp",
+                                 self._move(Direction.UP),
+                                 self._move_test(Direction.UP),
+                                 self._move_terminate(Direction.UP)
+                                 ),
+                          Option("MoveDown",
+                                 self._move(Direction.DOWN),
+                                 self._move_test(Direction.DOWN),
+                                 self._move_terminate(Direction.DOWN)
+                                 )
+                          ]
+                         )
+        self.player = player
+        self.playroom = playroom
+
+    def _move(self, direction: Direction) -> StateTransform:
+        def _move_dir(s: LowLevelState) -> LowLevelState:
+            return self.playroom.execute_no_change(s, (lambda: self.player.move(direction)))
+        return _move_dir
+
+    def _move_test(self, direction: Direction) -> StateTest:
+        def _move_test_dir(s: LowLevelState) -> bool:
+            return self.playroom.execute_no_change_return(s, (lambda: self.player.collide(direction)))
+        return _move_test_dir
+
+    def _move_terminate(self, direction: Direction) -> StateConsumer:
+        def _move_terminate_dir(s: LowLevelState) -> int:
+            return 1 if self.playroom.execute_no_change_return(s, (lambda: self.player.collide(direction))) else 0
+        return _move_terminate_dir
 
 
 def main():
     pr = Playroom()
     game = pr.game
-    for i in range(1000):
-        game.draw()
-        if not game.player.move(Direction.RIGHT):
-            break
-        print(pr.get_state())
-        time.sleep(0.01)
+    agent: Agent = PlayroomAgent(pr, game.player)
+    state = pr.get_state()
+    for i in range(500):
+        res: OptionExecuteReturn = agent.options[
+            rng.randint(0, 3)].execute(
+            state, (lambda s: None))
+        # print(pr.get_state(), res.state, res.time)
+        state = res.state
+        pr.draw_state(state)
+        # state = res.state
+        # print(pr.get_state())
+        # time.sleep(0.0001)
+    pr.draw_background()
+    for i in range(4):
+        print(agent.options[i].name)
+        init = agent.options[i].initiation()
+        eff = agent.options[i].effect()
+        for s in init:
+            pr.overlay_state(s, (0, 255, 0))
+        for s in eff:
+            pr.overlay_state(s, (255, 0, 0))
+        for e in agent.options[i].transitions:
+            pr.overlay_transition(e.start, e.dest, (255, 0, 0), (0, 255, 0))
+    input()
 
 
 if __name__ == "__main__":
